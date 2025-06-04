51a8b0a02c77fba6aafcbadc8574af705dee701b
timurasurov <timurasurov12@gmail.com>
2025-06-04 11:23:47 +0500

    Fix: add initialize summernote anywhere

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,44 @@
-jQuery(".summernote").summernote({
-  placeholder: "Hello stand alone ui",
-  tabsize: 2,
-  height: 300,
-  toolbar: [
-    ["style", ["style"]],
-    ["font", ["bold", "underline", "clear"]],
-    ["color", ["color"]],
-    ["para", ["ul", "ol", "paragraph"]],
-    ["table", ["table"]],
-    ["insert", ["link", "picture", "video"]],
-    ["view", ["fullscreen", "codeview", "help"]],
-  ],
+function initSummernoteElement(el) {
+  if (!jQuery(el).hasClass("summernote-initialized")) {
+    jQuery(el).summernote({
+      placeholder: "Add content here",
+      tabsize: 2,
+      height: 300,
+      toolbar: [
+        ["style", ["bold", "italic", "underline", "clear"]],
+        ["font", ["strikethrough", "superscript", "subscript"]],
+        ["para", ["ul", "ol", "paragraph"]],
+        ["table", ["table"]],
+        ["insert", ["link", "picture"]],
+        ["view", ["fullscreen", "codeview"]],
+      ],
+    });
+    jQuery(el).addClass("summernote-initialized");
+  }
+}
+
+jQuery(".summernote").each(function () {
+  initSummernoteElement(this);
+});
+
+const observer = new MutationObserver(function (mutationsList) {
+  for (const mutation of mutationsList) {
+    mutation.addedNodes.forEach(function (node) {
+      if (node.nodeType === 1) {
+        if (node.matches(".summernote")) {
+          initSummernoteElement(node);
+        } else {
+          // Вложенные summernote
+          node.querySelectorAll?.(".summernote").forEach(function (innerNode) {
+            initSummernoteElement(innerNode);
+          });
+        }
+      }
+    });
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
